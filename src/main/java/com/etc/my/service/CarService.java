9623d60a94d7fbaee4cfcbc86692a8cd2f750d5b
ljw <2432558976@qq.com>
2022-10-30 20:04:57 +0800

    更新了CarSerivce和PicSerivce

--- src/main/java/com/etc/my/service/CarService.java
+++ src/main/java/com/etc/my/service/CarService.java
@@ -1,9 +1,13 @@
 package com.etc.my.service;
 
 
+import com.etc.my.dto.CarAndPicDto;
 import com.etc.my.dto.CarDto;
 import com.etc.my.dto.PicDto;
 import com.etc.my.dto.PicDto2;
+import com.etc.my.entity.Car;
+
+import java.util.List;
 
 public interface CarService {
 
@@ -15,6 +19,55 @@ public interface CarService {
      */
     boolean insertCar(CarDto car);
 
-
+    /**
+     * 同时添加图片和车辆信息
+     * @param carDto
+     * @param dto
+     * @return
+     * @throws Exception
+     * author ljw
+     */
     boolean addCarAndPic(CarDto carDto, PicDto2 dto)throws Exception;
+
+    /**
+     * 分页查询车辆
+     * @param
+     * @return
+     * @author ljw
+     */
+    List<Car> queryCar(CarDto car);
+
+    /**
+     *查询车辆总条数
+     * @return
+     * @author ljw
+     */
+    Long getCarCount();
+
+    /**
+     * 根据车辆id查找车辆信息
+     * @param id
+     * @return
+     * @author ljw
+     */
+    CarAndPicDto queryCar(Integer id);
+
+    /**
+     * 修改车辆信息
+     * @param dto
+     * @return
+     * @author ljw
+     */
+    boolean updateCar(CarDto dto);
+
+
+    /**
+     * 同时修改图片和车辆信息
+     * @param carDto
+     * @param dto
+     * @return
+     * @throws Exception
+     * author ljw
+     */
+    boolean updateCarAndPic(CarDto carDto, PicDto2 dto)throws Exception;
 }
--- src/main/java/com/etc/my/service/PicService.java
+++ src/main/java/com/etc/my/service/PicService.java
@@ -5,6 +5,20 @@ import com.etc.my.dto.PicDto2;
 
 public interface PicService {
 
-
+    /**
+     * 添加图片库信息
+     * @param picDto
+     * @return
+     * @author ljw
+     *
+     */
     boolean insertPic(PicDto2 picDto);
+
+    /**
+     * 修改图片库信息
+     * @param pic
+     * @return
+     *  @author ljw
+     */
+    boolean updatePic(PicDto2 pic);
 }
