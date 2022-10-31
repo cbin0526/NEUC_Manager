3d3861468b452813d5fb591e01e5b641c7f23600
ljw <2432558976@qq.com>
2022-10-31 21:21:29 +0800

    提交防盗链的工具类

---
+++ src/main/java/com/etc/my/util/IsFish.java
@@ -0,0 +1,25 @@
+package com.etc.my.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IsFish {
+    /**
+     * 判断是否被盗链
+     *
+     * @param request
+     * @return
+     */
+    public boolean isFish(HttpServletRequest request) {
+        //获取网站的root
+        //获取当前项目名
+        String path = request.getContextPath();
+        //获取动态路径 http://ip:port/项目名/
+        String bathPath = request.getScheme() + "://" + request.getServerName();
+        System.out.println("当前项目的网络地址:"+bathPath);
+        //获取上个页面的地址
+        String url = request.getHeader("referer");
+        System.out.println("上一个页面的地址是"+url);
+        return url != null && url.startsWith(bathPath);
+
+    }
+}
