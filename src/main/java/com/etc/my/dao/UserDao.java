ebdb652516c636f0eefca8f6f815a7d3e32ec45f
ljw <2432558976@qq.com>
2022-10-31 17:11:14 +0800

    更新了UserDao

--- src/main/java/com/etc/my/dao/UserDao.java
+++ src/main/java/com/etc/my/dao/UserDao.java
@@ -8,7 +8,21 @@ public interface UserDao {
 
     List<User> queryAllUser();
 
+    /**
+     * 根据电话查询用户
+     * @param user_phone
+     * @return
+     * @author ljw
+     */
     User queryUser(String user_phone);
+
+    /**
+     * 根据名字和密码查询
+     * @param user_name
+     * @param user_pwd
+     * @return
+     */
+    User check(String user_name,String user_pwd);
 
 
 }
